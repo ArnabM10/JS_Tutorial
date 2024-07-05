1d38499ed11fe78a2445e03a0d7bf19d84476af9
Arnab Manna <110545895+ArnabM10@users.noreply.github.com>
2024-07-05 06:25:41 +0000

    datatypes

---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,29 @@
+"use strict";  // treat all JS code as newer version
+
+// alert(3 + 3) // we are using nodejs, not browser
+
+console.log(3+
+    3); // code readability should be high
+
+console.log("Arnab")
+
+let name  = "arnab"
+let age = 18
+let isLoogdIn = false
+let state;
+
+
+// <===========Primitive Datatypes=============>
+// number => 2^53 
+// bigint 
+// string => ""
+// boolean => true or false
+// null => standalone value
+// undefined => 
+// symbol => unique 
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null) // object
