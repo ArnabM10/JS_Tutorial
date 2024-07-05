dece0086b4c2964f732e15da259cd398d78c47c9
Arnab Manna <110545895+ArnabM10@users.noreply.github.com>
2024-07-05 06:40:14 +0000

    convesion of datatpes

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,26 @@
+let score = "arnab"
+
+// console.log(typeof score)
+// console.log(typeof(score));
+
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber);
+
+// "10" => 10
+// "10abc" => NaN
+// true => 1 , false => 0
+
+let isLoggedIn = "arnab" 
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 => true,  0 => false
+// "" => false
+// "arnab" => true
+
+let someNumber =  10
+let StringNumber = String(someNumber)
+console.log(StringNumber);
+console.log(typeof StringNumber);
